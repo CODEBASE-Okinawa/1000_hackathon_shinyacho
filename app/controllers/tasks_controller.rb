780f405d787a3d79112b79260d5eed1f9e418edc
yo-gari <jankieblackeymonkey8188@gmail.com>
2022-07-15 07:39:43 +0000

    Add model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,24 @@
 class TasksController < ApplicationController
-  def index
+  def new
+    @task = Task.new
     @tasks = Task.all
   end
 
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to index_url
+      redirect_to new_task_url
     else
-      render "index"
+      render "new"
     end
   end
 
   def update
     @task = Task.find(params[:id])
     if @task.update(task_params)
-      redirect_to index_url
+      redirect_to new_task_url
     else
-      render "index"
+      render "new"
     end
   end
 
