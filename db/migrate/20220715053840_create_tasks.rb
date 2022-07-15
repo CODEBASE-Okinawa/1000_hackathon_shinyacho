809906828cfd591b078605edbcbb5cac473e8d22
yo-gari <jankieblackeymonkey8188@gmail.com>
2022-07-15 06:47:11 +0000

    Add Controller View not Checkbox

--- db/migrate/20220715053840_create_tasks.rb
+++ db/migrate/20220715053840_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.1]
   def change
     create_table :tasks do |t|
       t.string :content
-      t.string :closed, default: false
+      t.string :closed, default: "false"
 
       t.timestamps
     end
