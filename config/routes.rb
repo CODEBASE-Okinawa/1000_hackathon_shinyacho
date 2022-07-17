d75a8c8fe0caac0d66c873ee61bec3610ee314e2
tobaryotaro <tobatobago@gmail.com>
2022-07-17 14:10:01 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root "task_lists#new"
   resources :tasks
   resources :task_lists
+  # get  '/help',    to: 'static_pages#help'
+  # get  '/about',   to: 'static_pages#about'
+  # get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_16_065715) do
+ActiveRecord::Schema.define(version: 2022_07_17_075054) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2022_07_16_065715) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "task_list_id", null: false
     t.index ["task_list_id"], name: "index_tasks_on_task_list_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "task_lists"
